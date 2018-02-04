e08b5ce3e184651b4afc8b62e7452442cba91553
Wael Elkadeem <elkadeem@hotmail.com>
2018-02-04 13:25:42 +0300

    Add ILogger Class to source control

---
+++ Common/BusinessSolutions.Common.Infra/Log/ILogger.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessSolutions.Common.Infra.Log
+{
+    public interface ILogger
+    {
+        void Error(Exception exception);
+
+        void Error(string error);
+
+        void Info(string message);
+    }
+}
